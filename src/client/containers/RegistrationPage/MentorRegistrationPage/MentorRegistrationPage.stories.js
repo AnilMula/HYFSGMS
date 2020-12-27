8b37f3b09651351657eaa30a0fb067ce8a54af38
AnilMula <anilreddymula@gmail.com>
2020-12-27 13:53:19 +0100

    empty

--- src/client/containers/RegistrationPage/MentorRegistrationPage/MentorRegistrationPage.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { MentorRegistrationPage } from './MentorRegistrationPage.component';
-
-export default {
-  title: 'MentorRegistrationPage',
-};
-
-export const MentorRegistration = () => <MentorRegistrationPage />;
